1fa6cbf09c3850d8cd5f3167f823cb2889cf04d2
ahmed fetouh <ahmed_fetouh2016@hotmail.com>
2022-11-02 11:55:59 +0300

    1111

--- Tranning-SPA/src/app/_Services/Auth.service.ts
+++ Tranning-SPA/src/app/_Services/Auth.service.ts
@@ -1,15 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
 import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  jwtHelper= new JwtHelperService();
   BaseUrl = 'https://localhost:7031/api/auth/';
-  decodedTokcken :any;
   constructor(private Http: HttpClient) {}
 
   Login(Model: any) {
@@ -18,25 +15,13 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
-          this.decodedTokcken=this.jwtHelper.decodeToken(user.token);
-          console.log(this.decodedTokcken);
         }
       })
     );
   }
 
-  Register(Model:any){
-    return this.Http.post(this.BaseUrl + "Register",Model);
-  }
-
-  LoggedIn()
+  Register(Model:any)
   {
-    try{
-    const token=localStorage.getItem('token')?.toString();
-    return ! this.jwtHelper.isTokenExpired(token);
-    }
-    catch{
-      return false;
-    }
+    return this.Http.post(this.BaseUrl + "Register",Model);
   }
 }
